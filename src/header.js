12280bc1830374b0009963f05c0a7b326d67c408
Robert Herhold <reherhold@gmail.com>
2016-05-23 08:14:16 -0400

    Add login button to navbar

--- src/header.js
+++ src/header.js
@@ -1,5 +1,27 @@
 'use strict';
 
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { AuthService } from './services/auth';
+import { Router } from 'aurelia-router';
+
 export class HeaderCustomElement {
-    
+
+    static inject = [AuthService, EventAggregator, Router];
+    constructor(auth, eventAggregator, router) {
+        this.auth = auth;
+        this.eventAggregator = eventAggregator;
+        this.router = router;
+    }
+
+    attached() {
+        this.showLogin = !this.isAuthenticated && this.router.currentInstruction.config.route !== 'login';
+        this.eventAggregator.subscribe('router:navigation:success', event => {
+            this.showLogin = !this.isAuthenticated && event.instruction.config.route !== 'login';
+        });
+    }
+
+    get isAuthenticated() {
+        return this.auth.isAuthenticated;
+    }
+
 }
