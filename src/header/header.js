22f10f5172f38d25a701085deeab4a3d9cfab221
Robert Herhold <reherhold@gmail.com>
2016-05-27 11:34:16 -0400

    Switch to my own auth service

--- src/header/header.js
+++ src/header/header.js
@@ -2,7 +2,7 @@
 
 import { HttpClient } from 'aurelia-http-client';
 import { EventAggregator } from 'aurelia-event-aggregator';
-import { AuthService } from 'aurelia-auth';
+import { AuthService } from '../services/auth';
 import { Router } from 'aurelia-router';
 
 export class HeaderCustomElement {
@@ -17,7 +17,6 @@ export class HeaderCustomElement {
 
     attached() {
         // Restore the profile from storage
-        this.profile = window.localStorage.profile ? JSON.parse(window.localStorage.profile) : null;
         // showLogin value for initial load
         this.showLogin = this._shouldShowLogin();
         // Recompute showLogin when changing pages (don't show it on the login page)
@@ -26,30 +25,17 @@ export class HeaderCustomElement {
         });
         // Make showLogin false when logged in
         this.eventAggregator.subscribe('auth:login', event => {
-            console.log("auth login event!");
-            console.log(event);
-            this.profile = event.selectedProfile;
-            this.profile.headImageURL = 'https://crafatar.com/avatars/' + this.profile.id + '?size=32';
-            window.localStorage.setItem('profile', JSON.stringify(event.selectedProfile));
             this.showLogin = false;
         });
         
         this.eventAggregator.subscribe('auth:logout', () => {
-            console.log("logout event");
-            this.profile = null;
             this.showLogin = this._shouldShowLogin();
         });
     }
 
     // Show the login button if the user is not already logged in and is not currently on the login page
     _shouldShowLogin() {
-        return !this.auth.isAuthenticated() && this.router.currentInstruction.config.route !== 'login';
-    }
-
-    // I am providing this getter rather than forcing the direct use of auth.isAuthenticated() because Aurelia doesn't seem to listen to
-    // changes to auth.isAuthenticated() for whatever reason
-    get isAuthenticated() {
-        return this.auth.isAuthenticated();
+        return !this.auth.isAuthenticated && this.router.currentInstruction.config.route !== 'login';
     }
 
 }
--- src/login/login.js
+++ src/login/login.js
@@ -1,25 +1,21 @@
 'use strict';
 
-import { AuthService } from 'aurelia-auth';
-import { EventAggregator } from 'aurelia-event-aggregator';
+import { AuthService } from '../services/auth';
 import { Router } from 'aurelia-router';
 
 export class Login {
 
-    static inject = [AuthService, EventAggregator, Router];
-    constructor(auth, eventAggregator, router) {
+    static inject = [AuthService, Router];
+    constructor(auth, router) {
         this.auth = auth;
-        this.eventAggregator = eventAggregator;
         this.router = router;
     }
 
     login() {
-        this.auth.login(this.email, this.password)
+        return this.auth.login(this.email, this.password)
             .then(res => {
                 // TODO make an alert w/ "Welcome, ______"
                 this.router.navigate('/');
-                // Publishing the event HAS to be after changing the page since the event listener depends on the current route
-                // this.eventAggregator.publish('auth:login', res);
             });
     }
 
