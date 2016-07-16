680dcb331f8959aee7cf47e50413e525b4a09291
Robert Herhold <reherhold@gmail.com>
2016-07-16 19:29:14 -0400

    Update for latest API

--- src/CustomElements/structure-blurb/structure-blurb.js
+++ src/CustomElements/structure-blurb/structure-blurb.js
@@ -10,17 +10,17 @@ import toastr from 'toastr';
 export class StructureBlurbCustomElement {
     @bindable structure;
     createStructureCmdPrefix = '/edifice create ';
-    
+
     static inject = [HttpClient, AuthService, EventAggregator];
     constructor(http, auth, eventAggregator) {
         this.http = http;
         this.auth = auth;
         this.eventAggregator = eventAggregator;
     }
-    
+
     attached() {
         new Clipboard('#buildStructureBtn');
-        
+
         this.structureIsStarred = this.auth.isAuthenticated && this.structure.stargazers.includes(this.auth.profile.app_metadata.mcuuid);
         // Reevaluate if the structure is starred upon login
         this.eventAggregator.subscribe('auth:login', event => {
@@ -38,22 +38,23 @@ export class StructureBlurbCustomElement {
             });
             return;
         }
+
+        // HACK Prematurely altering the stargazers before the request since it takes too long
+        if(this.structureIsStarred) {
+            this.structure.stargazers.splice(this.structure.stargazers.indexOf(this.auth.profile.app_metadata.mcuuid), 1);
+            this.structureIsStarred = false;
+        } else {
+            this.structure.stargazers.push(this.auth.profile.app_metadata.mcuuid);
+            this.structureIsStarred = true;
+        }
+
+        // TODO Maybe do something with the result
         return this.http.createRequest('/star')
             .asPost()
             .withContent({
-                structureId: this.structure._id
+                structureId: this.structure.id
             })
             .withHeader('Authorization', 'Bearer ' + this.auth.accessToken)
-            .send()
-            .then(res => {
-                // Rather than doing another request to get the number of stars, we'll manually increment/decrement it
-                if(this.structureIsStarred) {
-                    this.structure.stargazers.splice(this.structure.stargazers.indexOf(this.auth.profile.app_metadata.mcuuid), 1);
-                    this.structureIsStarred = false;
-                } else {
-                    this.structure.stargazers.push(this.auth.profile.app_metadata.mcuuid);
-                    this.structureIsStarred = true;
-                }
-            });
+            .send();
     }
 }
--- src/structure-view/structure-view.js
+++ src/structure-view/structure-view.js
@@ -20,22 +20,20 @@ export class StructureView {
 
     activate(params) {
         this.params = params;
-        let structureProm = this.http.get(`/structures/${this.params.id}?agent=EdificeWeb`)
+        return this.http.get(`/structures/${this.params.id}`)
             .then(response => {
                 this.structure = response.content;
                 return this.http.get('/playercache/' + this.structure.creatorUUID);
             }).then(playerProfileRes => {
                 this.structure.creatorName = playerProfileRes.content.name;
             });
-        let starsHistoryProm = this.http.get('/stats/' + this.params.id)
-            .then(response => {
-                this.starHistory = response.content;
-            }).catch(err => {
-                // This project has never been starred before
-                this.starHistory = null;
-            });
-
-        return Promise.all([structureProm, starsHistoryProm]);
+        // let starsHistoryProm = this.http.get('/stats/' + this.params.id)
+        //     .then(response => {
+        //         this.starHistory = response.content;
+        //     }).catch(err => {
+        //         // This project has never been starred before
+        //         this.starHistory = null;
+        //     });
     }
 
     attached() {
@@ -61,7 +59,7 @@ export class StructureView {
 
         // SAMPLE DATA
         // this.starHistory = {
-        //     _id : "5760cee643cf78b00e557cb3",
+        //     id : "5760cee643cf78b00e557cb3",
         //     structureId : "572df96ff989ea7e559c217d",
         //     values : {
         //         "2016" : {
@@ -81,59 +79,59 @@ export class StructureView {
         //         }
         //     }
         // }
-        if(!this.starHistory) {
-            // Never been starred or viewed before, although that shouldn't be possible since the client just viewed this page...
-            return;
-        }
-
-        let labels = []; // List of dates
-        let data = {
-            stars: [],
-            views: []
-        };
-        for(let year in this.starHistory.values) {
-            for(let month in this.starHistory.values[year]) {
-                for(let day in this.starHistory.values[year][month]) {
-                    let date = new Date(parseInt(year), parseInt(month), parseInt(day));
-                    date = moment(date);
-                    labels.push(date);
-                    for(let dataset of Object.keys(data)) {
-                        // It doesn't matter whether the value exists or not because there needs to be at least
-                        // the placeholder undefined for the chart to stay aligned
-                        data[dataset].push(this.starHistory.values[year][month][day][dataset]);
-                    }
-                }
-            }
-        }
-        let element = $('#structure-stats');
-        let chart = new Chart(element, {
-            type: 'line',
-            data: {
-                labels,
-                datasets: [{
-                    label: 'Stars',
-                    data: data.stars,
-                    backgroundColor: '#D4AF37'
-                }, {
-                    label: 'Views',
-                    data: data.views,
-                    backgroundColor: 'rgba(75,192,192,0.4)'
-                }]
-            },
-            options: {
-                scales: {
-                    xAxes: [{
-                        type: "time",
-                        stacked: true
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
+        // if(!this.starHistory) {
+        //     // Never been starred or viewed before, although that shouldn't be possible since the client just viewed this page...
+        //     return;
+        // }
+        //
+        // let labels = []; // List of dates
+        // let data = {
+        //     stars: [],
+        //     views: []
+        // };
+        // for(let year in this.starHistory.values) {
+        //     for(let month in this.starHistory.values[year]) {
+        //         for(let day in this.starHistory.values[year][month]) {
+        //             let date = new Date(parseInt(year), parseInt(month), parseInt(day));
+        //             date = moment(date);
+        //             labels.push(date);
+        //             for(let dataset of Object.keys(data)) {
+        //                 // It doesn't matter whether the value exists or not because there needs to be at least
+        //                 // the placeholder undefined for the chart to stay aligned
+        //                 data[dataset].push(this.starHistory.values[year][month][day][dataset]);
+        //             }
+        //         }
+        //     }
+        // }
+        // let element = $('#structure-stats');
+        // let chart = new Chart(element, {
+        //     type: 'line',
+        //     data: {
+        //         labels,
+        //         datasets: [{
+        //             label: 'Stars',
+        //             data: data.stars,
+        //             backgroundColor: '#D4AF37'
+        //         }, {
+        //             label: 'Views',
+        //             data: data.views,
+        //             backgroundColor: 'rgba(75,192,192,0.4)'
+        //         }]
+        //     },
+        //     options: {
+        //         scales: {
+        //             xAxes: [{
+        //                 type: "time",
+        //                 stacked: true
+        //             }],
+        //             yAxes: [{
+        //                 ticks: {
+        //                     beginAtZero: true
+        //                 }
+        //             }]
+        //         }
+        //     }
+        // });
     }
 
 }
