81e33de0d7f1bf212d683fdd33b6e4e4f1594665
Robert Herhold <reherhold@gmail.com>
2016-10-18 17:09:52 -0400

    Infinite scrolling on profile page

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -3,6 +3,7 @@
 import { HttpClient } from 'aurelia-http-client';
 import { StructurePaginator } from '../structure-paginator';
 import { PlayerProfileService } from '../services/playerprofile';
+import $ from 'jquery';
 
 export class Profile {
     
@@ -17,7 +18,7 @@ export class Profile {
     activate(params) {
         this.structurePaginator = new StructurePaginator(this.http, this.playerProfiles, {
             author: params.playerId,
-            limit: 9
+            limit: 6
         });
         
         let structuresProm = this.structurePaginator.contents().then(structures => this.structures = this.structures.concat(structures));
@@ -28,6 +29,18 @@ export class Profile {
             });
             
         return Promise.all([profileProm, structuresProm]);
+    }
+    
+    attached() {
+        // From http://stackoverflow.com/questions/5059526/infinite-scroll-jquery-plugin
+        this.scrollLoad = true;
+        $(window).scroll(() => { 
+            if (this.scrollLoad && $(window).scrollTop() >= $(document).height() - $(window).height() - 300) {
+                this.scrollLoad = false;
+                this.structurePaginator.nextPage();
+                this.structurePaginator.contents().then(structures => this.structures = this.structures.concat(structures)).then(() => this.scrollLoad = true);
+            }
+        });
     }
     
 }
